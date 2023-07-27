76b9c1ae00fa49ccd224ed85ba8c5e445a861c88
Prashant <prashant.sharma@symbtechnologies.com>
2023-07-28 00:42:57 +0530

    feat: project setup

---
+++ src/feature/constants/form.constants.ts
@@ -0,0 +1,23 @@
+export const FormFieldTypes = {
+    text: 'TEXT',
+    multiSelect: 'MULTI_SELECT',
+    singleSelect: 'SINGLE_SELECT',
+    dynamicSingleSelect: 'DYNAMIC_SINGLE_SELECT',
+    dynamicMultiSelect: 'DYNAMIC_MULTI_SELECT',
+    date: 'DATE',
+    dateTime: 'DATE_TIME',
+    editor: 'EDITOR',
+    radio: 'RADIO',
+    switch: 'SWITCH',
+    inputMinMax: 'INPUT_MIN_MAX',
+    inputNumber: 'INPUT_NUMBER',
+    customReorderInputList: 'CUSTOM_REORDER_INPUT_LIST',
+    customHeader: 'CUSTOM_FORM_HEADER',
+    fileUpload:"FILE_UPLOAD",
+    customAsyncSelect:"CUSTOM_ASYNC_SELECT",
+    customArrayInput:"CUSTOM_ARRAY_INPUT",
+    checkboxSingleInputSelect:"CHECKBOX_SINGLE_INPUT_SELECT",
+    textArea:"TEXT_AREA",
+    customBulkOrder:"CUSTOM_BULK_ORDER",
+    customImage:"CUSTOM_IMAGE"
+}
